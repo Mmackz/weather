f2e1b4d1f68166daa9f83ef815ac7c2ab82623aa
Mmackz <matthewmackenz@gmail.com>
2023-01-15 20:15:00 -0800

    🎨 format boilerplate code

--- src/context.js
+++ src/context.js
@@ -3,17 +3,12 @@ import { createContext, useState } from "react";
 const { Provider, Consumer } = createContext();
 
 function ContextProvider(props) {
-
    const [data, setData] = useState(1);
 
    const print = () => alert(data);
    const increment = () => setData((state) => state + 1);
 
-   return (
-      <Provider value={{data, print, increment}}>
-         {props.children}
-      </Provider>
-   )
+   return <Provider value={{ data, print, increment }}>{props.children}</Provider>;
 }
 
 export { ContextProvider, Consumer as ContextConsumer };
